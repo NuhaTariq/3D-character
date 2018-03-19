e91f5d93f0b1468f656b4ea16cf54ade395f5fd6
NuhaTariq <nuhaabulezz@hotmail.com>
2018-03-19 16:28:56 +0200

    char

---
+++ 3D chart/Assets/Student/Movment.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Movment : MonoBehaviour
+{
+    private Animator animComp;
+    private enum CharacterController { Ideal = 0, walking = 1, run = 2, jump = 3, sit = 4 };
+    private int movmentState;
+
+    void Start()
+    {
+        animComp = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            movmentState = (int)CharacterController.walking;
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            movmentState = (int)CharacterController.Ideal;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            movmentState = (int)CharacterController.run;
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            movmentState = (int)CharacterController.Ideal;
+        }
+        
+
+
+        animComp.SetInteger("StudentState", movmentState);
+    }
+}
+
---
+++ 3D chart/Assets/Student/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudentController : MonoBehaviour
+{
+    private Animator animComp;
+    void Start()
+    {
+        animComp = this.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        //walking
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            animComp.SetInteger("StudentState", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            animComp.SetInteger("StudentState", 0);
+        }
+        //running
+        if (Input.GetKeyDown(KeyCode.Space))
+         {
+           animComp.SetInteger("StudentState", 2);
+        }
+       if (Input.GetKeyUp(KeyCode.Space))
+         {
+           animComp.SetInteger("StudentState", 0);
+        }
+
+       //rotating left and right 
+        if (Input.GetKey("left"))
+        {
+            transform.Rotate(0, 5, 0);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(0, -5, 0);
+        }
+    }
+}
+
+
+
